e0e67709abf72a56b2bce94d42914ebc402633da
IAV-SiMr IAV-SiMr <silas.mueller@iav.de>
2023-07-07 13:15:00 +0000

    Added file change watch

---
+++ KataUnitTest/src/main.rs
@@ -0,0 +1,23 @@
+// src/main.rs
+// -------------
+
+fn adder(a: i64, b: i64) -> i64 {
+    return a + b;
+}
+
+
+
+// src/main.rs
+// -------------
+
+// ......
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_adder() {
+        assert_eq!(adder(2, 2), 5);
+    }
+}
