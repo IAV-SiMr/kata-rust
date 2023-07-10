6656b0e6a2cf5eaf40b858fa3957059382c3c1d0
IAV-SiMr IAV-SiMr <silas.mueller@iav.de>
2023-07-10 10:46:54 +0000

    Streamlined the file monitoring

--- OnFileChange.sh
+++ OnFileChange.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+# Usage: 
+# OnFileChange.sh PathToWatch IncludePattern Command
+# PathToWatch   : Monitor this path for file changes. The Command will be executed each time a file 
+#                 gets modified, moved, created or deleted within this path and all its subdirectories
+# IncludePattern: Only monitor files that match this pattern
+# Command       : This command will be executed each time a file in the monitored path (that matches the IncludePattern)
+#                 is modified, created, moved or deleted
+
+# install the inotifywait command if not already present
  sudo apt -y install inotify-tools
  
+ # sanity check whether the package was installed correctly
 if [ -z "$(which inotifywait)" ]; then
     echo "inotifywait not installed."
     echo "In most distros, it is available in the inotify-tools package."
@@ -9,18 +19,24 @@ fi
  
 counter=0;
  
+ # execute the command that was provided to the script
 function execute() {
     counter=$((counter+1))
     if [ $((counter%2)) -eq 1 ]; then
-        echo "Detected change n. $counter" 
-        eval "$@"
+        echo "########## Detected change ##########" 
+        eval $@
     fi
 }
  
-inotifywait --recursive --monitor --format "%e %w%f" \
---include '\.rs' \
---event modify,move,create,delete ./ \
+# start the monitoring with the configuration provided by the parameters
+inotifywait \
+    --recursive \
+    --monitor \
+    --format "%e %w%f" \
+    --include $2 \
+    --event modify,move,create,delete \
+    $1 \
 | while read changed; do
-    echo $changed
-    execute "$@"
+    # a change was detected, now execute the command
+    execute $3
 done
